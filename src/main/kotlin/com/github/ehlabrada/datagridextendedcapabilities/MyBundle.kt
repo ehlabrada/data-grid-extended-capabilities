0cfb60196246760450c4cc3d4df36a8ab0554c1e
GitHub Action <action@github.com>
2025-08-30 20:41:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/ehlabrada/datagridextendedcapabilities/MyBundle.kt
+++ src/main/kotlin/com/github/ehlabrada/datagridextendedcapabilities/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ehlabrada.datagridextendedcapabilities
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ehlabrada/datagridextendedcapabilities/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/ehlabrada/datagridextendedcapabilities/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.ehlabrada.datagridextendedcapabilities.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
